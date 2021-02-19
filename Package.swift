84d8e0b804c316e46e5998007cc18908933a0035
Dave Duprey <dave@what3words.com>
2021-02-19 11:43:09 +0000

    fixed a path in package definition

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
       .package(url: "https://github.com/what3words/w3w-swift-wrapper.git", "3.6.0"..<"4.0.0"),
-      .package(url: "https://github.com/what3words//w3w-swift-components.git", "2.0.0"..<"3.0.0")
+      .package(url: "https://github.com/what3words/w3w-swift-components.git", "2.0.0"..<"3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
